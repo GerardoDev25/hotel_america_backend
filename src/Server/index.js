93948243b5c83f6c8df4b4164de0158fcd03d010
Gerardo Miranda <gerardodev25@gmail.com>
2021-12-21 19:06:48 -0400

    server up

---
+++ src/Server/index.js
@@ -0,0 +1,25 @@
+import http from 'http';
+
+import express from 'express';
+import cors from 'cors';
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+    this.serverApp = http.createServer(this.app);
+    this.middlewares();
+  }
+
+  middlewares() {
+    this.app.use(cors());
+  }
+
+  listen() {
+    this.serverApp.listen(this.port, () => {
+      console.log('server run in port ', this.port);
+    });
+  }
+}
+
+export default Server;
