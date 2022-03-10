1627cd7c1366821f02a5101bf761b2d77c2f0443
Gerardo Miranda <gerardodev25@gmail.com>
2022-03-10 19:46:52 -0400

    update cafe router

--- src/routers/cafe.router.js
+++ src/routers/cafe.router.js
@@ -11,6 +11,6 @@ router.post('/where', Service.Cafe.getWhere);
 
 router.post('/', Validator.Cafe.create, Service.Cafe.cafeCreateAll);
 
-router.post('/:cafeId', Validator.Cafe.update, Service.Cafe.update);
+router.put('/:cafeId', Validator.Cafe.update, Service.Cafe.update);
 
 export default router;
